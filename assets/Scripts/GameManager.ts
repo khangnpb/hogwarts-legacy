0733065cd716cb33bec3c642cf75b3e4324325ff
Nguyen Phuc Binh Khang <91519610+khangnpb@users.noreply.github.com>
2023-03-29 22:05:40 +0700

    finish push

--- assets/Scripts/GameManager.ts
+++ assets/Scripts/GameManager.ts
@@ -1,85 +1,81 @@
-import { _decorator, Component, Node, Prefab, Button } from 'cc';
+import { _decorator, Component, Node, Prefab, Button } from "cc";
 const { ccclass, property } = _decorator;
-import * as cc from 'cc';
+import * as cc from "cc";
 
-@ccclass('GameManager')
+@ccclass("GameManager")
 export class GameManager extends Component {
-    @property(Prefab)
-    curPrefab: Prefab = null!;
+  @property(Prefab)
+  curPrefab: Prefab = null!;
 
-    @property(Node)
-    myNode: Node | null = null;
+  @property(Node)
+  myNode: Node | null = null;
 
-    start() {
-        // Attach the click event handler to the StartButton
-        const startButton = this.node.getChildByName("start");
-        const optionButton = this.node.getChildByName("option");
-        const aboutButton = this.node.getChildByName("about");
-        const exitButton = this.node.getChildByName("exit");
-        const loadingLabel = this.node.getChildByName("loading");
-        loadingLabel.active = false;
-        startButton.active = true;
-        optionButton.active = true;
-        aboutButton.active = true;
-        exitButton.active = true;
-        if (startButton) {
-            startButton.on(Node.EventType.TOUCH_END, this.onClickStartButton, this);
-        }
-        if(optionButton){
-            optionButton.on(Node.EventType.TOUCH_END, this.onClickOptionButton, this);
-        }
-        if(aboutButton){
-            aboutButton.on(Node.EventType.TOUCH_END,this.onClickAboutButton, this);
-        }
-        if(exitButton){
-            exitButton.on(Node.EventType.TOUCH_END,this.onClickExitButton, this);
-        }
+  start() {
+    // Attach the click event handler to the StartButton
+    const startButton = this.node.getChildByName("start");
+    const optionButton = this.node.getChildByName("option");
+    const aboutButton = this.node.getChildByName("about");
+    const exitButton = this.node.getChildByName("exit");
+    const loadingLabel = this.node.getChildByName("loading");
+    loadingLabel.active = false;
+    startButton.active = true;
+    optionButton.active = true;
+    aboutButton.active = true;
+    exitButton.active = true;
+    if (startButton) {
+      startButton.on(Node.EventType.TOUCH_END, this.onClickStartButton, this);
     }
-    
-    onClickStartButton() {
-        // Get all child nodes of the parent node
-        console.log("start button");
-        this.hideButton();
+    if (optionButton) {
+      optionButton.on(Node.EventType.TOUCH_END, this.onClickOptionButton, this);
+    }
+    if (aboutButton) {
+      aboutButton.on(Node.EventType.TOUCH_END, this.onClickAboutButton, this);
+    }
+    if (exitButton) {
+      exitButton.on(Node.EventType.TOUCH_END, this.onClickExitButton, this);
+    }
+  }
 
+  onClickStartButton() {
+    // Get all child nodes of the parent node
+    console.log("start button");
+    this.hideButton();
+  }
+  onClickOptionButton() {
+    console.log("option button");
+    this.hideButton();
+    const currentScene = cc.director.getScene();
+    if (currentScene.name !== "Scene") {
+      cc.director.loadScene("OptionScene", () => {
+        console.log("OptionScene loaded successfully");
+      });
     }
-    onClickOptionButton() {
-        console.log("option button");
-        this.hideButton();
-        const currentScene = cc.director.getScene();
-        if (currentScene.name !== 'Scene') {
-            cc.director.loadScene('OptionScene', () => {
-                console.log('OptionScene loaded successfully');
-            });
-        }
-        
+  }
+  onClickAboutButton() {
+    console.log("about button");
+    this.hideButton();
+    const currentScene = cc.director.getScene();
+    if (currentScene.name !== "Scene") {
+      cc.director.loadScene("About", () => {
+        console.log("AboutScene loaded successfully");
+      });
     }
-    onClickAboutButton(){
-        console.log("about button");
-        this.hideButton();
-        const currentScene = cc.director.getScene();
-        if (currentScene.name !== 'Scene') {
-            cc.director.loadScene('About', () => {
-                console.log('AboutScene loaded successfully');
-            });
-        }
-    }
-    onClickExitButton(){
-        console.log("exit button");
-        this.hideButton();
-        cc.game.end();
-    }
-    hideButton() {
-        const startButton = this.node.getChildByName("start");
-        const optionButton = this.node.getChildByName("option");
-        const aboutButton = this.node.getChildByName("about");
-        const exitButton = this.node.getChildByName("exit");
-        const loadingLabel = this.node.getChildByName("loading");
-        loadingLabel.active = true;
-        startButton.active = false;
-        optionButton.active = false;
-        aboutButton.active = false;
-        exitButton.active = false;
-    }
+  }
+  onClickExitButton() {
+    console.log("exit button");
+    this.hideButton();
+    cc.game.end();
+  }
+  hideButton() {
+    const startButton = this.node.getChildByName("start");
+    const optionButton = this.node.getChildByName("option");
+    const aboutButton = this.node.getChildByName("about");
+    const exitButton = this.node.getChildByName("exit");
+    const loadingLabel = this.node.getChildByName("loading");
+    loadingLabel.active = true;
+    startButton.active = false;
+    optionButton.active = false;
+    aboutButton.active = false;
+    exitButton.active = false;
+  }
 }
-
-
